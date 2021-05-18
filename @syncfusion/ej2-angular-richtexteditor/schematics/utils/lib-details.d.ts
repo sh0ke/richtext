282d35b6bf6d0ed5c204db42afa492452529a455
Torres <torres@kordata.mx>
2021-05-18 15:49:49 -0600

    compiled

--- @syncfusion/ej2-angular-richtexteditor/schematics/utils/lib-details.d.ts
+++
@@ -1,4 +0,0 @@
-export declare const pkgName = "@syncfusion/ej2-angular-richtexteditor";
-export declare const pkgVer = "^19.1.59";
-export declare const moduleName = "RichTextEditorModule";
-export declare const themeVer = "~19.1.59";
--- @syncfusion/ej2-angular-richtexteditor/schematics/utils/lib-details.ts
+++
@@ -1,4 +0,0 @@
-export const pkgName = '@syncfusion/ej2-angular-richtexteditor';
-export const pkgVer = '^19.1.59';
-export const moduleName = 'RichTextEditorModule';
-export const themeVer = '~19.1.59';
